e84b5211fc1a9f5eb1736a8676fd983309802795
eliran zach <thehobbit85@gmail.com>
2015-03-19 18:47:51 +0200

    Deleted one junk test

--- test/test.js
+++ test/test.js
@@ -1,16 +1,16 @@
-var assert = require('assert');
+var assert = require('assert')
 
-var bip38Decrypt = require(__dirname +  "/../lib/bip38-decrypt.js");
+var bip38Decrypt = require(__dirname +  "/../lib/bip38-decrypt.js")
 
-var encryptedPrivKey = '6PYUpKnHqAAzWSbjJCPwgB5wWnyYipmhqMysLDstE7H2gn9Pp8vtZrvNeS';
-var password = 'mozqswji00';
-var wrongPassword = '1234';
+var encryptedPrivKey = '6PYUpKnHqAAzWSbjJCPwgB5wWnyYipmhqMysLDstE7H2gn9Pp8vtZrvNeS'
+var password = 'mozqswji00'
+var wrongPassword = '1234'
 
 bip38Decrypt(encryptedPrivKey,password, function(err,decryptedPrivateWif) {
-  console.log(decryptedPrivateWif);
-  assert(decryptedPrivateWif === 'KwgWqrB6URob8jdFtaWYowH1gXtrxM5mnaEkr6WCDU2aJ1FUUvzC', 'Dosen\'t Works when password is right');
+  console.log(decryptedPrivateWif)
+  assert(decryptedPrivateWif === 'KwgWqrB6URob8jdFtaWYowH1gXtrxM5mnaEkr6WCDU2aJ1FUUvzC', 'Dosen\'t Works when password is right')
 });
 bip38Decrypt(encryptedPrivKey,wrongPassword, function(err,decryptedPrivateWif) {
-  console.log(err.message);
-  assert(err.message === "Wrong Password", 'Dosen\'t Works when password is wrong');
+  console.log(err.message)
+  assert(err.message === "Wrong Password", 'Dosen\'t Works when password is wrong')
 });
