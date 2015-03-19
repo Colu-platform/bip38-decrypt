fa51e9efc8cb515237e701e1dfb1b914fff503c6
eliran zach <thehobbit85@gmail.com>
2015-03-19 18:00:58 +0200

    Initial Commit

---
+++ test/test.js
@@ -0,0 +1,16 @@
+var assert = require('assert');
+
+var bip38Decrypt = require(__dirname +  "/../lib/bip38-decrypt.js");
+
+var encryptedPrivKey = '6PYUpKnHqAAzWSbjJCPwgB5wWnyYipmhqMysLDstE7H2gn9Pp8vtZrvNeS';
+var password = 'mozqswji00';
+var wrongPassword = '1234';
+
+bip38Decrypt(encryptedPrivKey,password, function(err,decryptedPrivateWif) {
+  console.log(decryptedPrivateWif);
+  assert(decryptedPrivateWif === 'KwgWqrB6URob8jdFtaWYowH1gXtrxM5mnaEkr6WCDU2aJ1FUUvzC', 'Dosen\'t Works when password is right');
+});
+bip38Decrypt(encryptedPrivKey,wrongPassword, function(err,decryptedPrivateWif) {
+  console.log(err.msg);
+  assert(err.msg === "Wrong Password", 'Dosen\'t Works when password is wrong');
+});
